9cf23bd10855e72789e7a8f1bd6bfb6980aeb533
Lucas Massi <lucasmassi18@gmail.com>
2020-12-06 22:04:53 -0300

    Developers: add migration for developers

---
+++ api/src/database/migrations/20201207010019-create-developers.js
@@ -0,0 +1,42 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('developers', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      age: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      url_linkedin: {
+        type: Sequelize.STRING
+      },
+      tecnologies: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('developers');
+  },
+};
